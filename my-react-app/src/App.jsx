bbfc0f197d5a005713851e8ef7b405d856ad916d
shauravbaramu <sauravmagar2000@gmail.com>
2025-01-18 17:05:08 -0500

    First React App

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header.jsx"
+
+function App() {
+
+  return(
+    <Header/>
+  );
+
+}
+  
+
+export default App
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,20 @@
+function Header() {
+  return (
+    <header>
+      <h1>My React App</h1>
+
+      <nav>
+        <ul>
+            <li>< a href="#">Home</a></li>
+            <li><a a href="#">About</a></li>
+            <li><a a href="#">Services</a></li>
+            <li><a a href="#">Contact</a></li>
+
+        </ul>
+      </nav>
+      <hr></hr>
+    </header>
+  );
+}
+
+export default Header;
